df6625ed67eb2a4a11b4a8c499c713034e6bda94
Marcel Mathé <m.mathe@gmx.net>
2019-02-27 08:59:41 +0100

    Spielkarten dokumentiert
    Minimale Javadoc-Kommentare geschrieben

--- src/bfw/risiko/Aufgabenkarte.java
+++ src/bfw/risiko/Aufgabenkarte.java
@@ -1,20 +1,36 @@
 package bfw.risiko;
 
+/**
+ * <h1>Aufgabenkarte</h1>
+ *
+ * @since 2019-02-25
+ */
 public class Aufgabenkarte extends Spielkarte {
+	/**
+	 * Die Aufgabe dieser Karte.
+	 */
 	private String aufgabe;
 
+	/**
+	 * Erstellt eine neue Aufgabenkarte.
+	 * @param aufgabe Die Aufgabe dieser Karte.
+	 */
 	public Aufgabenkarte(String aufgabe) {
 		this.aufgabe = aufgabe;
-		
 	}
-	
-	
-	
-	
+
+	/**
+	 * Gibt die Aufgabe dieser Karte zurück.
+	 * @return eine Aufgabe
+	 */
 	public String getAufgabe() {
 		return aufgabe;
 	}
 
+	/**
+	 * Ändert die Aufgabe dieser Karte.
+	 * @param aufgabe die neue Aufgabe
+	 */
 	public void setAufgabe(String aufgabe) {
 		this.aufgabe = aufgabe;
 	}
--- src/bfw/risiko/Laenderkarte.java
+++ src/bfw/risiko/Laenderkarte.java
@@ -1,27 +1,63 @@
 package bfw.risiko;
 
-public class Laenderkarte extends Spielkarte{
+/**
+ * <h1>Laenderkarte</h1>
+ * <p>
+ * Stellt ein {@code Land} dar.
+ * Besteht aus einem {@code Land} und einem {@code Symboltyp}.
+ *
+ * @since 2019-02-25
+ */
+public class Laenderkarte extends Spielkarte {
+	/**
+	 * Das Land für das die Karte steht.
+	 */
 	private Land land;
+
+	/**
+	 * Der Symboltyp der Karte (Infanterie, Kavallerie, Kanone).
+	 */
 	private Symboltyp symbol;
-	
-	
+
+	/**
+	 * Erstellt eine neue Laenderkarte.
+	 * @param land Das Land der Karte.
+	 * @param symbol der Typ der Karte
+	 */
 	public Laenderkarte(Land land, Symboltyp symbol) {
 		this.land = land;
 		this.symbol = symbol;
-		
+
 	}
-	
-	
-	
+
+	/**
+	 * Gibt das Land dieser Laenderkarte zurück.
+	 * @return ein Land
+	 */
 	public Land getLand() {
 		return land;
 	}
+
+	/**
+	 * Ändert das Land dieser Karte.
+	 * @param land ein Land.
+	 */
 	public void setLand(Land land) {
 		this.land = land;
 	}
+
+	/**
+	 * Gibt den Symboltyp dieser Laenderkarte zurück.
+	 * @return ein Symboltyp (Infanterie, Kavallerie, Kanone)
+	 */
 	public Symboltyp getSymbol() {
 		return symbol;
 	}
+
+	/**
+	 * Ändert den Symboltyp dieser Laenderkarte.
+	 * @param symbol ein Symboltyp (Infanterie, Kavallerie, Kanone)
+	 */
 	public void setSymbol(Symboltyp symbol) {
 		this.symbol = symbol;
 	}
