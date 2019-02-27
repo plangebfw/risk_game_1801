797132ba20b806ca7b1db8a2c7c538d148f9eda6
Marcel Mathé <m.mathe@gmx.net>
2019-02-27 11:06:51 +0100

    Besatzung und Symboltyp dokumentiert

--- src/bfw/risiko/Symboltyp.java
+++ src/bfw/risiko/Symboltyp.java
@@ -1,22 +1,41 @@
 package bfw.risiko;
 
+/**
+ * <h1>Symboltyp</h1>
+ * <p>
+ * Stellt den Symboltyp einer {@code Laenderkarte}
+ * und der {@code Jokerkarte} dar.
+ * <p>
+ * Mögliche Werte sind "Infanterie", "Kavallerie", "Kanone".
+ *
+ * @since 2019-02-25
+ */
 public class Symboltyp {
+	/**
+	 * Der Name des Symboltyps.
+	 */
 	private String name;
 
-	
+	/**
+	 * Erstellt einen neuen Symboltyp.
+	 * @param name der Name des Symboltyps
+	 */
 	public Symboltyp(String name) {
-		this.name = name;		
-		
+		this.name = name;
 	}
-	
-	
-	
-	
-	
+
+	/**
+	 * Gibt den Namen des Symboltyps zurück.
+	 * @return der Name des Symboltyps
+	 */
 	public String getName() {
 		return name;
 	}
 
+	/**
+	 * Ändert den Namen des Symboltyps.
+	 * @param name der neue Name des Symboltyps
+	 */
 	public void setName(String name) {
 		this.name = name;
 	}
